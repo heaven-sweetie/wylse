c920313afe6f581e79707542c6a891a864ecf041
Sunjae.Park <sunjae.park.kr@gmail.com>
2015-04-05 00:38:47 +0900

    FoodDetail - Tag 선택 구현

--- wylse/FoodDetailViewController.swift
+++ wylse/FoodDetailViewController.swift
@@ -23,6 +23,29 @@ class FoodDetailViewController: UIViewController {
         
         title = food.name
     }
+    
+    //MARK: - Action
+    @IBAction func newTagButtonTouched(sender: UIButton) {
+        let alertController = UIAlertController(title: "New Tag",
+            message: "Input tag name", preferredStyle: UIAlertControllerStyle.Alert)
+        alertController.addTextFieldWithConfigurationHandler { (textField:UITextField!) -> Void in
+            textField.returnKeyType = UIReturnKeyType.Done
+        }
+        alertController.addAction(UIAlertAction(title: "Cancel",
+            style: UIAlertActionStyle.Cancel, handler: nil))
+        alertController.addAction(UIAlertAction(title: "Add new tag",
+            style: UIAlertActionStyle.Default, handler: { (alertAction) -> Void in
+                if let textField:UITextField = alertController.textFields?.first as? UITextField,
+                    let tagName = textField.text {
+                        let newTag:Tag = Tag(name: tagName)
+                        tagList.append(newTag)
+                        
+                        self.foodTagTableView.reloadData()
+                }
+        }))
+        presentViewController(alertController, animated: true, completion: nil)
+    }
+    
 }
 
 let kFoodTagCell = "foodTagCell"
@@ -30,20 +53,36 @@ let kFoodTagCell = "foodTagCell"
 extension FoodDetailViewController: UITableViewDataSource {
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if let tags = food.tags {
-            return tags.count
-        } else {
-            return 0
-        }
+        return tagList.count
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         if let foodTagCell = tableView.dequeueReusableCellWithIdentifier(kFoodTagCell) as? UITableViewCell,
-            let tag = food.tags?[indexPath.row] as Tag! {
+            let tag = tagList[indexPath.row] as Tag! {
                 foodTagCell.textLabel?.text = tag.name
-            return foodTagCell
+                
+                if let tags = food.tags as [Tag]!,
+                    let selectedTag = tags.filter({ $0 == tag }).first as Tag! {
+                    foodTagCell.accessoryType = UITableViewCellAccessoryType.Checkmark
+                }
+                
+                return foodTagCell
         } else {
             return UITableViewCell()
+        }
+    }
+    
+}
+
+extension FoodDetailViewController: UITableViewDelegate {
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        if let tag = tagList[indexPath.row] as Tag! {
+            if let selectedTagList = food.tags as [Tag]! {
+                food.tags!.append(tag)
+            } else {
+                food.tags = [tag]
+            }
         }
     }
     
