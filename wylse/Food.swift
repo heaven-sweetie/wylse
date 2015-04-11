00fbce8625424c2f3b1e5e1d357976aa17593470
Sunjae.Park <sunjae.park.kr@gmail.com>
2015-04-11 16:56:11 +0900

    Model - 관계 변경 (Many to Many)

--- wylse/Food.swift
+++ wylse/Food.swift
@@ -2,15 +2,16 @@
 //  Food.swift
 //  wylse
 //
-//  Created by heaven on 3/21/15.
+//  Created by heaven on 4/11/15.
 //  Copyright (c) 2015 weirdmeetup. All rights reserved.
 //
 
 import Foundation
 import CoreData
 
-class Food : NSManagedObject {
+class Food: NSManagedObject {
+
     @NSManaged var name: String
-    @NSManaged var tag: Tag
-    
+    @NSManaged var tags: NSSet
+
 }
--- wylse/Tag.swift
+++ wylse/Tag.swift
@@ -2,14 +2,16 @@
 //  Tag.swift
 //  wylse
 //
-//  Created by heaven on 3/21/15.
+//  Created by heaven on 4/11/15.
 //  Copyright (c) 2015 weirdmeetup. All rights reserved.
 //
 
 import Foundation
 import CoreData
 
-class Tag : NSManagedObject {
+class Tag: NSManagedObject {
+
     @NSManaged var name: String
-    @NSManaged var food: Food
+    @NSManaged var foods: NSSet
+
 }
